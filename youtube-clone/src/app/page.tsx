3f11f17ceb21551de1f1812ac1da9d0bfaf3a7d2
DoDoxD <70909476+DoDoxD1@users.noreply.github.com>
2025-03-19 15:44:27 +0000

    project setup

---
+++ youtube-clone/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+import { DESTRUCTION } from "dns"
+export default function Home() {
+  return(
+    <div>
+      <p className="text-rose-500 font-bold">
+        Hello World!
+      </p>
+    </div>
+  )
+}
